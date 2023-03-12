9f8cfc9533e760bde063fdede2ef6323a5ebe891
Jeremias Puchulu <jeremiaspuchulu@gmail.com>
2023-03-12 20:10:46 -0300

    agregado de movimientos y salto

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class PlayerController : MonoBehaviour
+{
+    public float walkSpeed = 5f;
+    public float runSpeed = 10f;
+    public float jumpHeight = 1.9f;
+    public float gravityScale = -20f;
+
+    Vector3 moveInput = Vector3.zero;
+    CharacterController characterController;
+
+    private void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if (characterController.isGrounded)
+        {
+            moveInput = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+            if (Input.GetButtonDown("Sprint"))
+            {
+                moveInput = transform.TransformDirection(moveInput) * runSpeed;
+            }
+            else
+            {
+                moveInput = transform.TransformDirection(moveInput) * walkSpeed;
+            }
+
+            if (Input.GetButtonDown("Jump"))
+            {
+                moveInput.y = Mathf.Sqrt(jumpHeight + -2f * gravityScale);
+            }
+        }
+
+        moveInput.y += gravityScale * Time.deltaTime;
+        characterController.Move(moveInput * Time.deltaTime);
+    }
+
+    
+}
