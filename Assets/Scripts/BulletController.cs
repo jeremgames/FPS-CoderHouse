338d861c4257ab79ece095a5b4d83a7547bd3db4
JeremiasPuchulu <jeremiaspuchulu@gmail.com>
2023-03-13 02:12:33 -0300

    creacion de bullet

---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    public float bulletSpeed;
+    public Rigidbody rb;
+}
